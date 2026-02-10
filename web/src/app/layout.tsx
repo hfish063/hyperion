d56448e42abc2f2a70199435049f72008ebfaec9
haydenfish <hayden.fish@icloud.com>
2026-02-10 14:18:06 -0800

    Initial implementation of login/register pages

--- web/src/app/layout.tsx
+++ web/src/app/layout.tsx
@@ -1,11 +1,7 @@
+import "./globals.css";
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
-import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
-import { AppSidebar } from "@/components/app-side-bar";
-import AppHeader from "@/components/app-header";
 import { ThemeProvider } from "next-themes";
-import { Toaster } from "@/components/ui/sonner";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -24,27 +20,16 @@ export const metadata: Metadata = {
 
 export default function RootLayout({
   children,
-}: Readonly<{
+}: {
   children: React.ReactNode;
-}>) {
+}) {
   return (
     <html lang="en" suppressHydrationWarning>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <ThemeProvider attribute="class" defaultTheme="light">
-          <SidebarProvider>
-            <AppSidebar />
-            <main className="flex flex-col min-h-screen w-full p-4 space-y-4">
-              <div className="flex flex-row space-x-4 items-center">
-                <SidebarTrigger />
-                <AppHeader />
-              </div>
-              <hr />
-              {children}
-            </main>
-            <Toaster />
-          </SidebarProvider>
+        <ThemeProvider attribute="class" defaultTheme="system">
+          {children}
         </ThemeProvider>
       </body>
     </html>
