655cc6f21e182955ce565b262654b9dc02ba064f
haydenfish <hayden.fish@icloud.com>
2026-02-17 13:08:03 -0800

    Create header for auth pages

--- web/src/components/auth-header.tsx
+++ web/src/components/auth-header.tsx
@@ -1,10 +1,15 @@
 "use client";
 
+import { FlameIcon } from "lucide-react";
 import ThemeToggle from "./theme-toggle";
 
 export default function AuthHeader() {
   return (
-    <header className="flex flex-row space-x-4 items-center w-full justify-end p-4">
+    <header className="flex flex-row space-x-4 items-center w-full justify-between p-4">
+      <div className="flex flex-row space-x-2 items-center">
+        <FlameIcon />
+        <p className="text-2xl font-bold p-2">Hyperion</p>
+      </div>
       <ThemeToggle />
     </header>
   );
