7e6e799001a0bdef63e88cd9517b9bc5e0a7e432
haydenfish <hayden.fish@icloud.com>
2026-03-05 15:04:34 -0800

    Update BookCard UI

--- web/src/components/book/book-card.tsx
+++ web/src/components/book/book-card.tsx
@@ -1,46 +1,52 @@
 import Link from "next/link";
 import AddBookToLibraryButton from "./add-to-library-button";
 import { Card, CardContent } from "../ui/card";
-import CoverImage from "./cover-image";
 import { Button } from "../ui/button";
 import { Info } from "lucide-react";
 import { Book } from "@/app/api/book";
+import CoverImage from "./cover-image";
 
 export default function BookCard({
   metadata,
   bookExistsInLibrary,
 }: BookCardProps) {
-  return (
-    <Card>
-      <CardContent>
-        <div className="flex flex-col space-y-4">
-          <div className="flex flex-row justify-between items-center">
-            <div className="flex flex-row space-x-4 items-start flex-1 min-h-[80px]">
-              <CoverImage
-                coverImageUrl={metadata.coverEditionImageUrl}
-                title={metadata.title}
-                width={70}
-                height={100}
-              />
-              <BookCardContent metadata={metadata} />
-            </div>
+  const coverImageWidth = 100;
+  const coverImageHeight = 140;
 
-            <div className="flex flex-col space-y-4 items-start justify-center">
-              <AddBookToLibraryButton
-                data={metadata}
-                bookExistsInLibrary={bookExistsInLibrary}
-              />
-              <Link href={`/explore/${metadata.coverEditionId}`}>
-                <Button variant={"outline"}>
-                  <Info />
-                  Details
-                </Button>
-              </Link>
+  return (
+    <div className="flex flex-row space-x-4">
+      <CoverImage
+        coverImageUrl={metadata.coverEditionImageUrl}
+        title={metadata.title}
+        width={coverImageWidth}
+        height={coverImageHeight}
+      />
+      <Card className="flex-1">
+        <CardContent>
+          <div className="flex flex-col space-y-4">
+            <div className="flex flex-row justify-between items-center">
+              <BookCardContent metadata={metadata} />
+
+              <div className="flex flex-col space-y-4 items-start justify-center w-[100]px]">
+                <AddBookToLibraryButton
+                  data={metadata}
+                  bookExistsInLibrary={bookExistsInLibrary}
+                />
+                <Link
+                  href={`/explore/${metadata.coverEditionId}`}
+                  className="w-full"
+                >
+                  <Button variant="outline" className="w-full">
+                    <Info />
+                    Details
+                  </Button>
+                </Link>
+              </div>
             </div>
           </div>
-        </div>
-      </CardContent>
-    </Card>
+        </CardContent>
+      </Card>
+    </div>
   );
 }
 
