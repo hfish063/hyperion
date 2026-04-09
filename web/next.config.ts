0bae92b22740469cbdd8e585ca2e28e7831d5e94
haydenfish <hayden.fish@icloud.com>
2026-04-09 14:23:46 -0700

    Addd docker changes

--- web/next.config.ts
+++ web/next.config.ts
@@ -1,6 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  output: "standalone",
   images: {
     domains: ["covers.openlibrary.org", "assets.hardcover.app"],
   },
